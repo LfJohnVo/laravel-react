7f3463350855efdb6cbe5465b1f5e8258b16ef23
John Vo <vojohn95@gmail.com>
2023-09-25 17:09:36 -0600

    auth

--- backend/app/Http/Controllers/AuthController.php
+++ backend/app/Http/Controllers/AuthController.php
@@ -2,15 +2,85 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\RegisterRequest;
+use App\Http\Requests\UpdateInfoRequest;
+use App\Http\Requests\UpdatePasswordRequest;
+use App\Http\Resources\UserResource;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Symfony\Component\HttpFoundation\Response;
 use Essa\APIToolKit\Api\ApiResponse;
 
 class AuthController extends Controller
 {
     use ApiResponse;
 
-    public function hello()
+    public function register(Request $request)
     {
-        return $this->responseSuccess('Mensaje exitoso', 'Hello');
+        $user = User::create([
+            'first_name' => $request->input('first_name'),
+            'last_name' => $request->input('last_name'),
+            'email' => $request->input('email'),
+            'password' => Hash::make($request->input('password')),
+            //'role_id' => 1
+        ]);
+
+        return $this->responseCreated('User created', new UserResource($user));
+    }
+
+    public function login(Request $request)
+    {
+        if (!Auth::attempt($request->only('email', 'password'))) {
+            return $this->responseUnAuthenticated('Invalid credentials!');
+        }
+
+        /** @var User $user */
+        $user = Auth::user();
+
+        $jwt = $user->createToken('token')->plainTextToken;
+
+        $cookie = cookie('jwt', $jwt, 60 * 24);
+
+        return \response([
+            'jwt' => $jwt
+        ])->withCookie($cookie);
+    }
+
+    public function user(Request $request)
+    {
+        $user = $request->user();
+
+        return new UserResource($user->load('role'));
+    }
+
+    public function logout()
+    {
+        $cookie = \Cookie::forget('jwt');
+
+        return \response([
+            'message' => 'success'
+        ])->withCookie($cookie);
+    }
+
+    public function updateInfo(UpdateInfoRequest $request)
+    {
+        $user = $request->user();
+
+        $user->update($request->only('first_name', 'last_name', 'email'));
+
+        return $this->responseSuccess('User updated succesfuly', new UserResource($user));
+    }
+
+    public function updatePassword(UpdatePasswordRequest $request)
+    {
+        $user = $request->user();
+
+        $user->update([
+            'password' => Hash::make($request->input('password'))
+        ]);
+
+        return $this->responseSuccess('User updated succesfuly', new UserResource($user));
     }
 }
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -15,8 +15,9 @@ use App\Http\Controllers\AuthController;
 |
 */
 
-Route::get('hello', [AuthController::class, 'hello']);
+Route::post('register', [AuthController::class, 'register']);
+Route::post('login', [AuthController::class, 'login']);
 
-// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
+Route::middleware('auth:sanctum')->group(function(){
+    Route::get('user', [AuthController::class, 'user']);
+});
